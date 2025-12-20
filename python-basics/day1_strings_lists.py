139ae31947c98954777dbfad3b0a23cd0d043295
MohitShakya2025 <shakyamohit13032006@gmail.com>
2025-12-20 16:08:46 +0530

    Add string and list operations in day1_strings_lists.py

--- python-basics/day1_strings_lists.py
+++ python-basics/day1_strings_lists.py
@@ -1,1 +1,16 @@
+# String
+name="Mohit"
+college="ABV IIITM"
 
+print(name.upper())
+print(len(college))
+print(name[1:4])
+
+#List
+subject=["analog","python","verilog"]
+print(subject[1:3])
+print(subject)
+subject.insert(2,"matlab")
+print(subject)
+subject.append('sgnal and system')
+print(f"list of subjects after appending {subject}")
